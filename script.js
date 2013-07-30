23e3c28eb3b157b8096fafb2ef2ec8d5c849e578
James Velasco <jchvelasco@gmail.com>
2013-07-31 04:38:36 +0800

    make page

---
+++ script.js
@@ -0,0 +1,52 @@
+colorBox = {
+    conf: {
+        maxwidth: 200,
+        maxheight: 200,
+        minwidth: 30,
+        minheight: 30,
+        playfieldH: 600,
+        playfieldW: 800
+    },
+    createBoxes: function (count) {
+        var boxContainer = document.getElementById('playfield'),
+            frag = document.createDocumentFragment(),
+            divs,
+            el,
+            id = "colorbox";
+        for (var i = 0; i < count; i++) {
+            divs = document.createElement('div');
+            divs.className = id + (i + 1) + ' box';
+            divs.setAttribute("style",
+                "background-color: " + colorBox.randomHex() + "; " +
+                "width: " + colorBox.randomWidth() + "px; " +
+                "height: " + colorBox.randomHeight() + "px; " +
+                "top: " + colorBox.randomPosY() + "px; " +
+                "left: " + colorBox.randomPosX() + "px;"
+                );
+            frag.appendChild(divs);
+            boxContainer.appendChild(frag);
+        }
+    },
+    randomWidth: function () {
+        return Math.floor(Math.random() * (colorBox.conf.maxwidth - colorBox.conf.minwidth + 1) + colorBox.conf.minwidth + 1);
+    },
+    randomHeight: function () {
+        return Math.floor(Math.random() * (colorBox.conf.maxheight - colorBox.conf.minheight + 1) + colorBox.conf.minheight + 1);
+    },
+    randomHex: function () {
+        var letters = "0123456789ABCDEF".split(''),
+            color = "#";
+
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 15)];
+        }
+
+        return color;
+    },
+    randomPosY: function () {
+        return Math.floor(Math.random() * (colorBox.conf.playfieldH - colorBox.conf.maxheight));
+    },
+    randomPosX: function () {
+        return Math.floor(Math.random() * (colorBox.conf.playfieldW - colorBox.conf.maxwidth));
+    }
+};
